f60cf1e020eac68a2d6015f49c6738ae8d6a9a0b
Connor-Wilkins <connor.wilkins@stu.ocsb.ca>
2025-10-06 18:44:03 +0000

    added comments

---
+++ main.ts
@@ -0,0 +1,9 @@
+/**
+ * @author Connor Wilkins
+ * @fileoverview this is the traditional "Hello, World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone.");
